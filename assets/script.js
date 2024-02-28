26cf2bd68c31d944ccde7b8e982b64c612e08627
Julienne H <juliennehattabi@gmail.com>
2024-02-28 10:32:14 +0100

    added bullet point

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,38 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 var arrowLeft = document.querySelector(".arrow_left");
-arrowLeft.addEventListener("click", function(){
-	alert ("flèche gauche");
+arrowLeft.addEventListener("click", function () {
+	alert("flèche gauche");
 });
 
 var arrowRight = document.querySelector(".arrow_right");
-arrowRight.addEventListener("click", function(){
+arrowRight.addEventListener("click", function () {
 	alert("flèche droite");
 });
+
+const dots = document.querySelector(".dots");
+
+for (const i = 0; i < slides.length; i++) {
+	const dot = document.createElement("div");
+	dot.classList.add("dot");
+	dots.appendChild(dot);
+
+};
+
+const dotSelected = document.querySelector(".dot_selected");
